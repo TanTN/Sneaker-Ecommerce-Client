f63242ad4aef4c4fb210ddeec0cf051d0e7dc244
TanTN12 <ttan6596@gmail.com>
2023-11-29 21:40:28 +0700

    add

--- src/layout/components/navbar/User/index.jsx
+++ src/layout/components/navbar/User/index.jsx
@@ -76,9 +76,7 @@ const User = () => {
                             alt={userCurrent.name}
                             className="border-[1px] border-[#a02222]"
                             sx={{ height: 30, width: 30 }}
-                        >
-                            {userCurrent?.name[0]?.toUpperCase()}
-                        </Avatar>
+                        />
                     ) : (
                         <Avatar alt="avt" sx={{ height: 30, width: 30 }} className="border-[1px] border-[#a02222]" />
                     )}
